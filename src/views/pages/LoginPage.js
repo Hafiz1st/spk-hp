a2c08128bf2f0bf683d311bd5373873c26c0610f
Hafiz1st <hafiz100st@gmail.com>
2021-07-08 17:39:42 +0800

    Fix login texts

--- src/views/pages/LoginPage.js
+++ src/views/pages/LoginPage.js
@@ -7,6 +7,13 @@ const LoginPage = {
     ${HeaderElement.render({ title: 'Login' })}
     <main>
     <h1>Sistem Pendukung Keputusan ( SPK ) Pemilihan Handphone Terbaik dengan Metode SAW berbasis Web Javascript</h1>
+    Fishburn (1967) dan MacCrimmon (1968). Metode Simple Additive Weighting (SAW) sering juga dikenal istilah metode penjumlahan terbobot. Konsep dasar metode SAW adalah mencari penjumlahan terbobot dari rating kinerja pada setiap alternatif pada semua atribut.
+
+Metode SAW membutuhkan proses normalisasi matriks keputusan (X) ke suatu skala yang dapat diperbandingkan dengan semua rating alternatif yang ada.
+
+Menurut Fachmi Basyaib (2006) Metode Simple Additive Weighting (SAW) merupakan metode paling dikenal dan paling banyak digunakan orang dalam menghadapi situasi Multi Attribute Decision Making (MADM). metode ini mengharuskan pembuat keputusan menentukan bobot bagi setiap attribut. skor total untuk sebuah alternatif diperoleh dengan menjumlahkan seluruh hasil perkalian antar rating (yang dapat dibandingkan lintas attribut) dan bobot tiap attribut. rating tiap atribut haruslah bebas dimensi dalam arti telah melewati proses normalisasi sebelumnya.
+
+Metode SAW sering juga dikenal istilah metode penjumlahan terbobot. Konsep dasar metode SAW adalah mencari penjumlahan terbobot dari rating kinerja pada setiap alternatif pada semua atribut. Metode SAW membutuhkan proses normalisasi matriks keputusan (X) ke suatu skala yang dapat diperbandingkan dengan semua rating alternatif yang ada.
     <form id='login'>
     <input name='id' type='text' placeholder='Id'>
     <input name='password' type='password' placeholder='Password'>
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, './docs'),
     filename: 'bundle.js',
   },
   devServer: {
     host: '0.0.0.0',
-    contentBase: path.resolve(__dirname, './public/'),
+    contentBase: path.resolve(__dirname, './docs/'),
     compress: false,
     hot: true,
     port: 8080,
